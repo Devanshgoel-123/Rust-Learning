911c8e73bb57f6d27e6e2d88d9e69e4924d42de4
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-08-06 21:39:59 +0530

    Mutable Refernce

--- learningRust/src/main.rs
+++ learningRust/src/main.rs
@@ -1,10 +1,20 @@
+// fn main() {
+//     let s1 = String::from("hello");
+//     let s2 = takes_ownership(s1);
+//     println!("{}", s2);
+// }
+
+// fn takes_ownership(some_string: String) -> String {
+//     println!("{}", some_string);
+//     return some_string; // return the string ownership back to the original main fn
+// }
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = takes_ownership(s1);
-    println!("{}", s2);
+    let mut s = String::from("hello");
+    change(&mut s);
 }
 
-fn takes_ownership(some_string: String) -> String {
-    println!("{}", some_string);
-    return some_string; // return the string ownership back to the original main fn
+fn change(some_string: &mut String) {
+    //reference to a mutable data must be called using &mut data-type
+    some_string.push_str(", world");
 }
+//we can only have a single mutable reference or multiple immutable reference so as to protect memory safe feature opf Rust.
