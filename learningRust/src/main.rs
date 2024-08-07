4342dc30bb8c708558b045ad7be3cd1b0fa15ad0
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-08-07 19:22:12 +0530

    Learned about enums,optionEnum,error handling

--- learningRust/src/main.rs
+++ learningRust/src/main.rs
@@ -1,26 +1,16 @@
-// // fn main() {
-// //     let s1 = String::from("hello");
-// //     let s2 = takes_ownership(s1);
-// //     println!("{}", s2);
-// // }
-
-// // fn takes_ownership(some_string: String) -> String {
-// //     println!("{}", some_string);
-// //     return some_string; // return the string ownership back to the original main fn
-// // }
-// fn main() {
-//     let mut s = String::from("hello");
-//     change(&mut s);
-// }
-
-// fn change(some_string: &mut String) {
-//     //reference to a mutable data must be called using &mut data-type
-//     some_string.push_str(", world");
-// }
-// //we can only have a single mutable reference or multiple immutable reference so as to protect memory safe feature opf Rust.
+fn find_first_a(s: String) -> Option<i32> {
+    for (index, character) in s.chars().enumerate() {
+        if character == 'a' {
+            return Some(index as i32);
+        }
+    }
+    return None;
+}
 
 fn main() {
-    let s: String = String::from("Hello World");
-    let bytes_s: Vec<u8> = s.bytes().collect();
-    println!("String in bytes is :{:?}", bytes_s);
+    let my_string = String::from("raman");
+    match find_first_a(my_string) {
+        Some(index) => println!("The letter 'a' is found at index: {}", index),
+        None => println!("The letter 'a' is not found in the string."),
+    }
 }
