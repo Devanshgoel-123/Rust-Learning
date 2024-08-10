68230d4a09482837926ff16ec36cff58cafbd1ec
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-08-10 12:13:09 +0530

    Learning about vectors and Strings

--- learningRust/src/main.rs
+++ learningRust/src/main.rs
@@ -1,6 +1,11 @@
 pub mod helpers;
+pub mod vector;
+use vector::vectors_learning;
 //This of the type crate::FileName::functionName
 fn main() {
-    let answer = helpers::name_helpers::get_fullname("Devansh".to_string(), "Goel".to_string());
-    println!("Hello to {}", answer);
+    // let answer = helpers::name_helpers::get_fullname("Devansh".to_string(), "Goel".to_string());
+    // println!("Hello to {}", answer);
+    // vectors_learning::test_vec_int();
+    // vectors_learning::test_vec_string();
+    vectors_learning::test_vec_car();
 }
---
+++ learningRust/src/vector.rs
@@ -0,0 +1,36 @@
+pub mod vectors_learning {
+    use core::str;
+
+    pub fn test_vec_int() {
+        let mut my_ints: Vec<i32> = Vec::new(); //necessary to specify type so rust knows what type is stored here
+        let mut my_ints2 = vec![1, 2, 3, 4, 5];
+        my_ints.push(2);
+        my_ints.push(3);
+
+        println!("THe number is : {}", &my_ints[0]); // One way to get the number
+        println!("THe number is : {:?}", my_ints.get(0));
+        println!("The capacity of vector is :{}", my_ints.capacity());
+    }
+
+    pub fn test_vec_string() {
+        let names = vec!["Devansh", "Goel", "Aditya", "Random"];
+        for name in names {
+            println!("The name of the person is : {}", name);
+        }
+    }
+    #[derive(Debug)]
+    struct Car {
+        manufacturer: String,
+        model: String,
+    }
+
+    pub fn test_vec_car() {
+        let mut my_car = vec![Car {
+            manufacturer: "Maruti".to_string(),
+            model: "Ertiga".to_string(),
+        }];
+        println!("My car type is : {:?}", my_car.get(0).unwrap().manufacturer);
+        let value_removed = &my_car.remove(0);
+        println!("{:?}", value_removed);
+    }
+}
