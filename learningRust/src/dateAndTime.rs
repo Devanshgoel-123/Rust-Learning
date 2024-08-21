abaa378f9141878d6855294a68e7d601569e57bd
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-08-21 12:12:21 +0530

    Learned about the dateandTIme chrono crate

--- learningRust/src/dateAndTime.rs
+++ learningRust/src/dateAndTime.rs
@@ -1,5 +1,8 @@
 use std::ops::Sub;
+use std::thread::sleep;
 use std::time::{Duration, Instant};
+
+use chrono::{DateTime, Local, Utc};
 
 pub fn test_std_time() {
     let dur_one = Duration::from_millis(10000);
@@ -8,4 +11,14 @@ pub fn test_std_time() {
     println!("{:?}", dur_one.as_millis()); //prints 1
     println!("{:?}", dur_one.as_secs()); //prints 10
     println!("{:?}", dur_three.as_secs()); //prints 5
+
+    let now = Instant::now();
+    sleep(Duration::new(2, 0)); //sleeps the program for 2 mins
+    let later = now.elapsed();
+    println!("{}", later.as_secs());
+
+    let local_now: DateTime<Local> = chrono::Local::now(); //also has the data type UTC which is 5and half hours behind IST
+    println!("{}", local_now.format("%d %m %y %z"));
+    let utc_now: DateTime<Utc> = chrono::Utc::now(); //also has the data type UTC which is 5and half hours behind IST
+    println!("{}", utc_now.format("%d %m %y %z"));
 }
